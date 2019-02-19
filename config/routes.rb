e7e2b5e8638e8ddf175d831f6ddd9c9b40bf64f6
Edoardo Olivieri <diddo.42@hotmail.it>
2019-02-19 17:00:32 +0000

    added only controller view model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'articles', to: 'articles#index'
+
+  get 'articles', to: 'articles#new'
+
+  get 'articles/:id', to: 'articles#show', as: 'article'
+
+  post 'task', to: 'articles#create'
+
+  get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+
+  patch 'articles/:id', to: 'articles#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
